32c0ec004edf37aacc3aeefc1df6d5bf7f33d0b9
mcarena <mcarena.quijada@outlook.com>
2024-02-10 15:52:14 -0300

    Segundo comit donde se realiza segundo ejercicio de javascript de celsius, kelvin, fahrenheir

---
+++ assets/js/ejercicio2.js
@@ -0,0 +1,14 @@
+
+let temperaturaCelsius = parseInt(prompt("Ingresa la temperatura en grados Celsius:"));
+let temperaturaKelvin = temperaturaCelsius + 273.15;
+let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
+
+    console.log("Temperatura en grados Celsius: " + temperaturaCelsius + "°C");
+    console.log("Temperatura en grados Kelvin: " + temperaturaKelvin + " K");
+    console.log("Temperatura en grados Fahrenheit: " + temperaturaFahrenheit + " °F");
+
+
+
+
+
+
