abf868b2368f163979213c4f700c0b68364f0f14
mcarena <mcarena.quijada@outlook.com>
2024-02-10 16:12:17 -0300

    Cuerto commit que hace referencia al ejercicio 4 donde se ingresan 5 numeros y se calcula la suma y el promedio

---
+++ assets/js/ejercicio4.js
@@ -0,0 +1,15 @@
+
+
+const num1 = prompt("Ingrese 1 numero:");
+const num2 = prompt("Ingrese 2 numero:");
+const num3 = prompt("Ingrese 3 numero:");
+const num4 = prompt("Ingrese 4 numero:");
+const num5 = prompt("Ingrese 5 numero:");
+
+
+let suma = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4) + parseInt(num5);
+let promedio = suma / 5;
+
+console.log("Suma de los números: " + suma);
+console.log("Promedio de los números: " + promedio);
+
